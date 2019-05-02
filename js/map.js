3f5016163ec1fed99c3e832b276106ce8569e56d
OndraMarek <czondrejmarek@gmail.com>
2019-05-02 21:40:36 +0200

    0.5

--- js/map.js
+++ js/map.js
@@ -1,15 +1,14 @@
 $(function(){
-    $('h2').text('Největší mrakodrapy');
     $('.objekt').attr({'style':'fill:rgba(0,0,255,0.8);'});
 
     $('.objekt').on('click',function(){
         $('.objekt').attr({'style':'fill:rgba(0,0,255,0.8);'});
         $(this).attr({'style':'fill:rgba(0,255,255,0.5);stroke:black'});
-        $('#text h2').text($().data('nazev'));
+        $('#text h2').text($(this).data('nazev'));
         $('#text .liRok').text($(this).data('rok'));
         $('#text .liMesto').text($(this).data('mesto'));
         $('#text .liStat').text($(this).data('stat'));
         $('#text .liVyska').text($(this).data('vyska'));
-        $('img').attr('src',$(this).data('fotka'));
+        $('main img').attr('src',$(this).data('fotka'));
     })
 })
